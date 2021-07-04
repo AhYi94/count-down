bcaedc3db79fabc3bddb54602a5c41d25d398df5
Adam <lingyunggie@gmail.com>
2021-07-04 19:01:01 +0800

    base

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <h1>Hello World </h1>
+      <Counter />
+  </div>
+</template>
+
+<script>
+import Counter from '~/components/Counter.vue'
+
+export default {
+    components: {
+        Counter
+    }
+}
+</script>
+
+<style>
+
+</style>
